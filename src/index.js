dd97f56a5aa9641d11f8ced742d21c30c36ebc64
jonghyouk yun <ageldama@gmail.com>
2021-03-14 23:19:58 +0900

    --wip-- [skip ci]

--- src/index.js
+++ src/index.js
@@ -2,41 +2,81 @@ import { Subject, of } from 'rxjs'
 import { flatMap, switchMap, map, catchError } from 'rxjs/operators'
 import $ from 'jquery'
 
-function addResult(n) {
-  $('#results').append(`<li>${n}</li>`)
-}
+class Incrementor {
+  constructor(resultsElemId) {
+    this.resultsElemId = resultsElemId
 
-function filterNum(n) {
-  if (n % 3 === 0) {
-    throw new Error(`multiple of three! ${n}`)
+    this.incCount1$ = new Subject()
+    this.initIncCount1$()
+
+    this.incCount2$ = new Subject()
+    this.initIncCount2$()
   }
-  return n
-}
 
-const incCount$ = new Subject()
+  addResult(n) {
+    $(this.resultsElemId).append(`<li>${n}</li>`)
+  }
 
-incCount$
-  .pipe(
-    switchMap((n) => {
-      return of(n).pipe(
-        map(filterNum),
-        catchError((err) => {
-          console.log('ERR', err)
-          return of('HANDLED')
+  filterNum(n) {
+    if (n % 3 === 0) {
+      throw new Error(`multiple of three! ${n}`)
+    }
+    return n
+  }
+
+  initIncCount1$() {
+    this.incCount1$
+      .pipe(
+        switchMap((n) => {
+          return of(n).pipe(
+            map(this.filterNum),
+            catchError((err) => {
+              console.log('ERR', err)
+              return of('HANDLED')
+            })
+          )
         })
       )
-    })
-  )
-  .subscribe(
-    addResult,
-    (err) => console.log('GOT ERR', err),
-    () => console.log('COMPLETE')
-  )
+      .subscribe(
+        (n) => this.addResult(n),
+        (err) => console.log('GOT ERR', err),
+        () => console.log('COMPLETE')
+      )
+  }
+
+  initIncCount2$() {
+    this.incCount2$
+      .pipe(
+        flatMap((n) => {
+          return of(n).pipe(
+            map(this.filterNum),
+            catchError((err) => {
+              console.log('ERR', err)
+              return of('HANDLED')
+            })
+          )
+        })
+      )
+      .subscribe(
+        (n) => this.addResult(n),
+        (err) => console.log('GOT ERR', err),
+        () => console.log('COMPLETE')
+      )
+  }
+}
 
 $(() => {
-  let count = 0
+  const incrementor1 = new Incrementor('#results1')
+  let count1 = 0
 
-  $('#doItBtn').click(() => {
-    incCount$.next(++count)
+  $('#doItBtn1').click(() => {
+    incrementor1.incCount1$.next(++count1)
+  })
+
+  const incrementor2 = new Incrementor('#results2')
+  let count2 = 0
+
+  $('#doItBtn2').click(() => {
+    incrementor2.incCount2$.next(++count2)
   })
 })
