498e29f8c80fd21f308ca7a6903657d8cfb22abc
jonghyouk yun <ageldama@gmail.com>
2021-03-14 17:28:44 +0900

    --wip-- [skip ci]

--- src/index.js
+++ src/index.js
@@ -1,4 +1,45 @@
-import { Subject, from, BehaviorSubject } from "rxjs";
-import { flatMap, map, tap } from "rxjs/operators";
-import lodash from "lodash";
+import { Subject, of, onErrorResumeNext } from "rxjs"
+import { flatMap, switchMap, map, tap, catchError } from "rxjs/operators"
+import $ from 'jquery'
 
+function addResult(n) {
+  $('#results').append(`<li>${n}</li>`)
+}
+
+function filterNum(n) {
+  if (n % 3 == 0) {
+    throw new Error('multiple of three!')
+  }
+  return n
+}
+
+const incCount$ = new Subject()
+onErrorResumeNext(
+incCount$.pipe(
+  switchMap(n => {
+    return of(n).pipe(
+      map(filterNum),
+      catchError(err => {
+        console.log('ERR', err)
+        return of('HANDLED')
+      })
+    )
+  }),
+)).subscribe(
+  addResult,
+  err => {
+    console.log('GOT ERR', err)
+  },
+  () => {
+    console.log('COMPLETE')
+  })
+
+$(() => {
+
+  let count = 0
+
+  $('#doItBtn').click(() => {
+    incCount$.next(++count)
+  })
+
+})
